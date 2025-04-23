4a9c2c9d54629e5cd42d5a3494246de07c89d201
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-23 16:11:39 +0600

    create authorguestlinks component

---
+++ resources/js/components/header/AuthOrGuestLinks.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ShoppingCart, User } from "lucide-react";
+import { usePage } from "@inertiajs/react";
+import CustomLink from './CustomLink';
+
+export default function AuthOrGuestLinks() {
+    const { props } = usePage();
+
+    return (
+        <>
+            <CustomLink href={"/cart"} label={<ShoppingCart />} />
+
+            <div className="w-px h-6 rounded-full bg-neutral-900 opacity-60"></div>
+
+            {props.user ? (
+                <CustomLink href={"/user"} label={<User />} />
+            ) : (
+                <>
+                    <CustomLink href={"/login"} label={"Login"} />
+                    <CustomLink href={"/register"} label={"Register"} />
+                </>
+            )}
+        </>
+    );
+}
