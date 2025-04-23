c737e8353d99a73ee55570cfa6e039edce363692
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-23 16:08:19 +0600

    create home page

---
+++ resources/js/Pages/Home/Index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RootLayout from "../../components/layouts/RootLayout";
+
+export default function Index() {
+    return (
+        <RootLayout>
+            <h1>Home Page</h1>
+        </RootLayout>
+    );
+}
