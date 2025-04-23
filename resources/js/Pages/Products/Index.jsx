ef170f1a6dec91a2fcc84d782a8e6c27f3972d2c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-23 16:12:42 +0600

    create products page component

---
+++ resources/js/Pages/Products/Index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RootLayout from "../../components/layouts/RootLayout";
+
+export default function Index() {
+    return (
+        <RootLayout>
+            <h1>Products Page</h1>
+        </RootLayout>
+    );
+}
