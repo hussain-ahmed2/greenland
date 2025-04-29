531a19ec1dbd6415ba8ec23a299c37ac2b8fcb08
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-29 20:08:03 +0600

    add best selling component to home page

--- resources/js/Pages/Home/Index.jsx
+++ resources/js/Pages/Home/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import RootLayout from "../../components/layouts/RootLayout";
 import Banner from "../../components/home/Banner";
+import BestSellingPlants from "../../components/home/BestSellingPlants";
 
 export default function Index() {
     return (
         <RootLayout>
             <Banner />
+            <BestSellingPlants />
         </RootLayout>
     );
 }
