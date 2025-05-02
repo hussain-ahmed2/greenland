24f2242e734c5f9cc71d03196a436bc4b33a05de
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-05-02 07:55:57 +0600

    add about us component

--- resources/js/Pages/Home/Index.jsx
+++ resources/js/Pages/Home/Index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import RootLayout from "../../components/layouts/RootLayout";
 import Banner from "../../components/home/Banner";
 import BestSellingPlants from "../../components/home/BestSellingPlants";
+import AboutUs from "../../components/home/AboutUs";
 
 export default function Index() {
     return (
         <RootLayout>
             <Banner />
             <BestSellingPlants />
+            <AboutUs />
         </RootLayout>
     );
 }
