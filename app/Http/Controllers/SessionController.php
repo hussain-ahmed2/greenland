f88a9bea92cf167cddcdfec1c76a7193d5cf424f
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-05-06 14:14:49 +0600

    add create and store function

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Email;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
     public function index() {
-        return inertia('User/Index');
+        return inertia('Auth/DashBoard');
+    }
+
+    public function create() {
+        return inertia('Auth/Login');
+    }
+
+    public function store(Request $request) {
+        $user = $request->validate([
+            'email' => ['required', Email::defaults()],
+            'password' => ['required', Password::defaults()]
+        ]);
+
+        if (! Auth::attempt($user)) {
+            throw ValidationException::withMessages([
+                'email' => 'sorry, those credentials do not match.'
+            ]);
+        }
+
+        $request->session()->regenerate();
+        
+        return redirect(route('user.dashboard'));
     }
 }
