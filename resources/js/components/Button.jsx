ad2fe17d21a1c0c91673d9bd3bc273781e22d5f0
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-05-06 14:22:36 +0600

    create custom button component

---
+++ resources/js/components/Button.jsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import React from "react";
+
+export default function Button({
+    label = "Submit",
+    type = "button",
+    onClick = () => {},
+    className = "",
+    disabled = false
+}) {
+    return (
+        <button
+            className={clsx(
+                "font-medium px-6 py-3 bg-green-500 text-white rounded-md hover:bg-green-600 active:bg-green-600 active:scale-[.98] focus:ring-4 active:ring-4 ring-green-500/50 transition duration-300",
+                className
+            )}
+            onClick={onClick}
+            type={type}
+            disabled={disabled}
+        >
+            {label}
+        </button>
+    );
+}
