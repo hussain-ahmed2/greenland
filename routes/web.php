4d96daa100c3ee9852f90b0b7416ef857c4d546c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-05-06 14:20:12 +0600

    require auth routes file auth.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Home/Index');
-Route::inertia('/products', 'Products/Index');
+Route::inertia('/', 'Home/Index')->name('home');
+Route::inertia('/products', 'Products/Index')->name('products');
+
+require __dir__.'/auth.php';
