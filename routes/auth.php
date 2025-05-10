0038997a8f305b4b6fa848ef46d492a71d0b9f2c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-05-10 18:14:15 +0600

    feat: add route names and logout endpoint to auth routes

--- routes/auth.php
+++ routes/auth.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function () {
-    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
     Route::post('/register', [RegisteredUserController::class, 'store']);
 
-    Route::get('/login', [SessionController::class, 'create']);
+    Route::get('/login', [SessionController::class, 'create'])->name('login');
     Route::post('/login', [SessionController::class, 'store']);
 });
 
 Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
     Route::get('/dashboard', [AuthController::class, 'index'])->name('user.dashboard');
 });
